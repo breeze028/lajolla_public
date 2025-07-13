462b9c689ee7dff8c6b81c0aefb00496bfde4ffb
breeze028 <1575470525@qq.com>
2025-07-13 23:15:33 +0800

    finish vol_path_tracer_1

--- src/vol_path_tracing.h
+++ src/vol_path_tracing.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "scene.h"
+#include "pcg.h"
+#include "transform.h"
+
 // The simplest volumetric renderer: 
 // single absorption only homogeneous volume
 // only handle directly visible light sources
@@ -7,7 +11,35 @@ Spectrum vol_path_tracing_1(const Scene &scene,
                             int x, int y, /* pixel coordinates */
                             pcg32_state &rng) {
     // Homework 2: implememt this!
-    return make_zero_spectrum();
+    int w = scene.camera.width, h = scene.camera.height;
+    Vector2 screen_pos((x + next_pcg32_real<Real>(rng)) / w,
+                       (y + next_pcg32_real<Real>(rng)) / h);
+    Ray ray = sample_primary(scene.camera, screen_pos);
+    // For this homework, we will not use ray differentials.
+    RayDifferential ray_diff = RayDifferential{Real(0), Real(0)};
+
+    std::optional<PathVertex> vertex_ = intersect(scene, ray, ray_diff);
+    if (!vertex_) {
+        // Hit background.
+        return make_zero_spectrum();
+    }
+    PathVertex vertex = *vertex_;
+
+    Spectrum radiance = make_zero_spectrum();
+
+    // Compute the transmittance through the medium.
+    int medium_id = vertex.exterior_medium_id;
+    Medium medium = scene.media[medium_id];
+    Spectrum sigma_a = get_sigma_a(medium, vertex.position);
+    Real t_hit = distance<double>(vertex.position, xform_point(scene.camera.cam_to_world, Vector3{0, 0, 0})); 
+    Spectrum transmittance = exp(-sigma_a * t_hit);
+    
+    if (is_light(scene.shapes[vertex.shape_id])) {
+        // We hit a light immediately.
+        radiance += transmittance * emission(vertex, -ray.dir, scene);
+        return radiance;
+    }
+    return radiance;
 }
 
 // The second simplest volumetric renderer: 
